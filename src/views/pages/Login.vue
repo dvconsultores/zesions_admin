59b3eaf2f854f7312729071bec8b4549969944ec
AngelArevalo <aarevalo@dvconsultores.com>
2023-12-19 11:52:15 -0400

    Primera Revision Angel

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -25,84 +25,38 @@
 
         <!-- login form -->
         <v-card-text>
-          <v-form>
+          <v-form @submit="login()">
             <v-text-field
-              v-model="user"
+              v-model="username"
               outlined
-              label="Email"
+              label="Email or Username"
               hide-details
               class="mb-3"
             ></v-text-field>
 
             <v-text-field
-              v-model="passw"
+              v-model="password"
               outlined
-              :type="isPasswordVisible ? 'text' : 'password'"
+              :type="hide_password ? 'password' : 'text'"
               label="Password"
-              placeholder="············"
-              :append-icon="isPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
+              placeholder="**********"
+              :append-icon="hide_password ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
               hide-details
-              @click:append="isPasswordVisible = !isPasswordVisible"
+              @click:append="hide_password = !hide_password"
+              @
             ></v-text-field>
-
-            <!-- <div class="d-flex align-center justify-space-between flex-wrap">
-              <v-checkbox
-                label="Remember Me"
-                hide-details
-                class="me-3 mt-1"
-              >
-              </v-checkbox> -->
-
-              <!-- forgot link -->
-              <!-- <a
-                href="javascript:void(0)"
-                class="mt-1"
-              >
-                Forgot Password?
-              </a>
-            </div> -->
 
             <v-btn
               block
               color="primary"
               class="mt-6"
-              @click="login()"
+              type="submit"
             >
               Iniciar sesion
             </v-btn>
           </v-form>
         </v-card-text>
 
-        <!-- create new account  -->
-        <!-- <v-card-text class="d-flex align-center justify-center flex-wrap mt-2">
-          <span class="me-2">
-            New on our platform?
-          </span>
-          <router-link :to="{name:'pages-register'}">
-            Create an account
-          </router-link>
-        </v-card-text> -->
-
-        <!-- divider -->
-        <!-- <v-card-text class="d-flex align-center mt-2">
-          <v-divider></v-divider>
-          <span class="mx-5">or</span>
-          <v-divider></v-divider>
-        </v-card-text> -->
-
-        <!-- social links -->
-        <!-- <v-card-actions class="d-flex justify-center">
-          <v-btn
-            v-for="link in socialLink"
-            :key="link.icon"
-            icon
-            class="ms-1"
-          >
-            <v-icon :color="$vuetify.theme.dark ? link.colorInDark : link.color">
-              {{ link.icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-actions> -->
       </v-card>
     </div>
 
@@ -133,43 +87,11 @@
 
 <script>
 // eslint-disable-next-line object-curly-newline
-import { mdiFacebook, mdiTwitter, mdiGithub, mdiGoogle, mdiEyeOutline, mdiEyeOffOutline } from '@mdi/js'
-import { ref } from '@vue/composition-api'
+import { mdiEyeOutline, mdiEyeOffOutline } from '@mdi/js'
 
 export default {
   setup() {
-    const isPasswordVisible = ref(false)
-    const email = ref('')
-    const password = ref('')
-    const socialLink = [
-      {
-        icon: mdiFacebook,
-        color: '#4267b2',
-        colorInDark: '#4267b2',
-      },
-      {
-        icon: mdiTwitter,
-        color: '#1da1f2',
-        colorInDark: '#1da1f2',
-      },
-      {
-        icon: mdiGithub,
-        color: '#272727',
-        colorInDark: '#fff',
-      },
-      {
-        icon: mdiGoogle,
-        color: '#db4437',
-        colorInDark: '#db4437',
-      },
-    ]
-
     return {
-      isPasswordVisible,
-      email,
-      password,
-      socialLink,
-
       icons: {
         mdiEyeOutline,
         mdiEyeOffOutline,
@@ -178,8 +100,9 @@ export default {
   },
   data() {
     return {
-      user: '',
-      passw: '',
+      hide_password: true, 
+      username: '',
+      password: '',
     }
   },
   mounted() {
@@ -187,10 +110,7 @@ export default {
   },
   methods: {
     login() {
-      const user = this.user;
-      const clave = this.passw;
-      this.axios.post('/login/', { username: user, password: clave }).then((res) => {
-        console.log(res.data)
+      this.axios.post('/login/', { username: this.username, password: this.password }).then((res) => {
         localStorage.dataUser = JSON.stringify(res.data)
         localStorage.Authorization = res.data.token
         localStorage.permisos = JSON.stringify(res.data.permisos)
@@ -199,7 +119,6 @@ export default {
       }).catch((err) => {
         console.log(err)
       });
-      // console.log(user, clave)
     },
   },
 }
